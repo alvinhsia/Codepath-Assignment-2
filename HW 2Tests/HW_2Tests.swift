bff2b3c06351330d1fd5bf4a43cff4398cb46743
Alvin Hsia <alvinhsia@gmail.com>
2016-02-09 19:58:01 -0800

    Initial Commit

---
+++ HW 2Tests/HW_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HW_2Tests.swift
+//  HW 2Tests
+//
+//  Created by Alvin Hsia on 2/9/16.
+//  Copyright © 2016 Alvin Hsia. All rights reserved.
+//
+
+import XCTest
+@testable import HW_2
+
+class HW_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
